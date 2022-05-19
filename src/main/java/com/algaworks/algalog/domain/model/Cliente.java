54a2056681a8471fdba814f50e7bbf2e509762bd
Kauê Sabino VIllaça de Almeida <kauevillaca2018@gmail.com>
2022-05-19 16:48:25 -0300

    campos validados por tamanho e conteúdo

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,17 +1,35 @@
 package com.algaworks.algalog.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import java.util.Objects;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Cliente {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Column(name = "nome")
+	@Size(max = 60)
 	private String name;
+	
+	@Email
+	@Size(max = 255)
 	private String email;
+	
+	@Column(name = "telefone")
+	@NotBlank
+	@Size(max = 20)
 	private String telephone;
 	public Long getId() {
 		return id;
@@ -37,5 +55,23 @@ public class Cliente {
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 	
 }
