9e42ade9b583138d94ad0ac9009411af8340be90
Kauê Sabino VIllaça de Almeida <kauevillaca2018@gmail.com>
2022-05-19 16:49:06 -0300

    busca por email

--- src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
@@ -1,5 +1,17 @@
 package com.algaworks.algalog.domain.repository;
 
-public interface ClienteRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algalog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNameContaining(String nome);
+	Optional<Cliente> findByEmail(String email);
 
 }
