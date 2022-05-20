4412e3ef727b6c66d0f6fef700cfafe6a71828ca
Kauê Sabino VIllaça de Almeida <kauevillaca2018@gmail.com>
2022-05-20 11:37:56 -0300

    Entidades refeitas, tabela criada por Flyway

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -2,41 +2,68 @@ package com.algaworks.algalog.domain.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
 public class Entrega {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private BigDecimal taxa;
-	private LocalDateTime dataPedido;
-	private LocalDateTime dataFinalizacao;
-	
-	@Enumerated(EnumType.STRING)
-	private StatusEntrega status;
 	
 	@ManyToOne
-	private Cliente cliente;
+	private Cliente clente;
 	
 	@Embedded
 	private Destinatario destinatario;
 	
+	
+	private BigDecimal taxa;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusEntrega status;
+	private LocalDateTime dataPedido;
+	private LocalDateTime dataFinalizacao;
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public Cliente getClente() {
+		return clente;
+	}
+	public void setClente(Cliente clente) {
+		this.clente = clente;
+	}
+	public Destinatario getDestinatario() {
+		return destinatario;
+	}
+	public void setDestinatario(Destinatario destinatario) {
+		this.destinatario = destinatario;
 	}
 	public BigDecimal getTaxa() {
 		return taxa;
 	}
 	public void setTaxa(BigDecimal taxa) {
 		this.taxa = taxa;
+	}
+	public StatusEntrega getStatus() {
+		return status;
+	}
+	public void setStatus(StatusEntrega status) {
+		this.status = status;
 	}
 	public LocalDateTime getDataPedido() {
 		return dataPedido;
@@ -50,23 +77,22 @@ public class Entrega {
 	public void setDataFinalizacao(LocalDateTime dataFinalizacao) {
 		this.dataFinalizacao = dataFinalizacao;
 	}
-	public StatusEntrega getStatus() {
-		return status;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
-	public void setStatus(StatusEntrega status) {
-		this.status = status;
-	}
-	public Cliente getCliente() {
-		return cliente;
-	}
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
-	public Destinatario getDestinatario() {
-		return destinatario;
-	}
-	public void setDestinatario(Destinatario destinatario) {
-		this.destinatario = destinatario;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entrega other = (Entrega) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
